95df30b58cff46ea0212365ae683f32a14424128
Axel29 <axel.bouaziz@hotmail.fr>
2015-03-11 18:40:17 +0100

    Génération CRUD Commentaires + Correction erreur bazinga_faker

---
+++ src/Esgi/BlogBundle/Form/CommentsType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Esgi\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CommentsType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('commentPostId')
+            ->add('commentAuthor')
+            ->add('commentCreatedAt')
+            ->add('commentUpdatedAt')
+            ->add('commentTitle')
+            ->add('commentContent')
+            ->add('commentApprouved')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Esgi\BlogBundle\Entity\Comments'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'esgi_blogbundle_comments';
+    }
+}
