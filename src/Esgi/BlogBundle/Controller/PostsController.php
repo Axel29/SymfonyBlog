6a2537311acac39553faa61882308c7dc6b8fdd5
Axel29 <axel.bouaziz@hotmail.fr>
2015-03-11 18:26:53 +0100

    Ajout CRUD posts + nettoyage projet suppression fichiers / répertoires inutiles.

---
+++ src/Esgi/BlogBundle/Controller/PostsController.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace Esgi\BlogBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Esgi\BlogBundle\Entity\Posts;
+use Esgi\BlogBundle\Form\PostsType;
+
+/**
+ * Posts controller.
+ *
+ */
+class PostsController extends Controller
+{
+
+    /**
+     * Lists all Posts entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('EsgiBlogBundle:Posts')->findAll();
+
+        return $this->render('EsgiBlogBundle:Posts:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new Posts entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Posts();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('esgi_blog_post_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('EsgiBlogBundle:Posts:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a Posts entity.
+     *
+     * @param Posts $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Posts $entity)
+    {
+        $form = $this->createForm(new PostsType(), $entity, array(
+            'action' => $this->generateUrl('esgi_blog_post_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Posts entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Posts();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('EsgiBlogBundle:Posts:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Posts entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('EsgiBlogBundle:Posts')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Posts entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('EsgiBlogBundle:Posts:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Posts entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('EsgiBlogBundle:Posts')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Posts entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('EsgiBlogBundle:Posts:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Posts entity.
+    *
+    * @param Posts $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Posts $entity)
+    {
+        $form = $this->createForm(new PostsType(), $entity, array(
+            'action' => $this->generateUrl('esgi_blog_post_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Posts entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('EsgiBlogBundle:Posts')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Posts entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('esgi_blog_post_edit', array('id' => $id)));
+        }
+
+        return $this->render('EsgiBlogBundle:Posts:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Posts entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('EsgiBlogBundle:Posts')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Posts entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('esgi_blog_post'));
+    }
+
+    /**
+     * Creates a form to delete a Posts entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('esgi_blog_post_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
