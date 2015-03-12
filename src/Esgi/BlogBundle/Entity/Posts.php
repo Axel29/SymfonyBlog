98f04ab8b76cadc515d402971636921c29a0ba8d
Axel29 <axel.bouaziz@hotmail.fr>
2015-03-12 14:40:57 +0100

    Changement colonne "post_status" en type int

--- src/Esgi/BlogBundle/Entity/Posts.php
+++ src/Esgi/BlogBundle/Entity/Posts.php
@@ -58,7 +58,7 @@ class Posts
     /**
      * @var string
      *
-     * @ORM\Column(name="post_status", type="string", length=255)
+     * @ORM\Column(name="post_status", type="integer")
      */
     private $postStatus;
 
@@ -233,7 +233,7 @@ class Posts
     /**
      * Get postStatus
      *
-     * @return string 
+     * @return integer
      */
     public function getPostStatus()
     {
